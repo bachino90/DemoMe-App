05c63d3eb484766cb1f799564a8074d04961cfdd
bachino90 <emilianobivachi@gmail.com>
2013-12-06 12:25:19 -0300

    Initial Commit

---
+++ DemoMe/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  DemoMe
+//
+//  Created by Emiliano Bivachi on 06/12/13.
+//  Copyright (c) 2013 Emiliano Bivachi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ DemoMeTests/DemoMeTests.m
@@ -0,0 +1,34 @@
+//
+//  DemoMeTests.m
+//  DemoMeTests
+//
+//  Created by Emiliano Bivachi on 06/12/13.
+//  Copyright (c) 2013 Emiliano Bivachi. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DemoMeTests : XCTestCase
+
+@end
+
+@implementation DemoMeTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
